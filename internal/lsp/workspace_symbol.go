37232aa6083bdac23e2d49f462d7908c948e4253
Daisuke Suzuki <daisuzu@gmail.com>
2019-12-11 22:08:47 +0900

    internal/lsp: improve dup check

--- internal/lsp/workspace_symbol.go
+++ internal/lsp/workspace_symbol.go
@@ -2,7 +2,6 @@ package lsp
 
 import (
 	"context"
-	"fmt"
 	"go/ast"
 	"path/filepath"
 	"sort"
@@ -16,12 +15,14 @@ func (s *Server) workspaceSymbol(ctx context.Context, params *protocol.Workspace
 	ctx, done := trace.StartSpan(ctx, "lsp.Server.workspaceSymbol")
 	defer done()
 
-	viewNames := make([]string, len(s.session.Views()))
-
-	symbolsMap := map[string]protocol.SymbolInformation{}
-	for i, view := range s.session.Views() {
-		viewNames[i] = view.Folder().Filename()
+	pkgsMap := make(map[string]struct{})
+	var symbols []protocol.SymbolInformation
+	for _, view := range s.session.Views() {
 		for _, pkg := range view.Snapshot().KnownPackages(ctx) {
+			if _, ok := pkgsMap[pkg.PkgPath()]; ok {
+				continue
+			}
+			pkgsMap[pkg.PkgPath()] = struct{}{}
 			for _, handle := range pkg.CompiledGoFiles() {
 				file, mapper, err1, err2 := handle.Parse(ctx)
 				if err1 != nil || err2 != nil {
@@ -40,15 +41,14 @@ func (s *Server) workspaceSymbol(ctx context.Context, params *protocol.Workspace
 							if err != nil {
 								return false
 							}
-							location := protocol.Location{
-								URI:   protocol.DocumentURI("file://" + handle.File().Identity().URI.Filename()),
-								Range: rng,
-							}
-							symbolsMap[fmt.Sprintf("%v", location)] = protocol.SymbolInformation{
-								Name:     t.Name,
-								Kind:     protocol.File,
-								Location: location,
-							}
+							symbols = append(symbols, protocol.SymbolInformation{
+								Name: t.Name,
+								Kind: protocol.File,
+								Location: protocol.Location{
+									URI:   protocol.DocumentURI("file://" + handle.File().Identity().URI.Filename()),
+									Range: rng,
+								},
+							})
 						}
 						return false
 					}
@@ -58,16 +58,17 @@ func (s *Server) workspaceSymbol(ctx context.Context, params *protocol.Workspace
 		}
 	}
 
-	symbols := make([]protocol.SymbolInformation, 0, len(symbolsMap))
-	for _, v := range symbolsMap {
-		symbols = append(symbols, v)
+	if len(symbols) == 0 {
+		return nil, nil
 	}
+
 	sort.Slice(symbols, func(i, j int) bool {
 		li := strings.Index(symbols[i].Name, params.Query) + len(symbols[i].Name)
 		lj := strings.Index(symbols[j].Name, params.Query) + len(symbols[j].Name)
 		if li == lj {
-			di := pkgDistance(symbols[i].Location.URI, viewNames)
-			dj := pkgDistance(symbols[j].Location.URI, viewNames)
+			dir := s.session.Views()[0].Folder().Filename()
+			di := pkgDistance(symbols[i].Location.URI, dir)
+			dj := pkgDistance(symbols[j].Location.URI, dir)
 			if di == dj {
 				if symbols[i].Location.URI == symbols[j].Location.URI {
 					return symbols[i].Location.Range.Start.Line < symbols[j].Location.Range.Start.Line
@@ -81,17 +82,12 @@ func (s *Server) workspaceSymbol(ctx context.Context, params *protocol.Workspace
 	return symbols, nil
 }
 
-func pkgDistance(uri string, names []string) int {
-	if len(names) == 0 {
-		return -1
-	}
-
+func pkgDistance(uri, dir string) int {
 	path := filepath.Dir(strings.ReplaceAll(uri, "file://", ""))
-	if path == names[0] {
+	if path == dir {
 		return 0
 	}
 
-	dir := names[0]
 	for i := 1; i <= 3; i++ {
 		if strings.HasPrefix(path, dir) {
 			return i
